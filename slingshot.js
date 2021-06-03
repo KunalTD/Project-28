7e6eae57194b35f98cccffb06ec124ae6df29007
KunalTD <75251465+KunalTD@users.noreply.github.com>
2021-06-03 17:06:16 +0530

    Add files via upload

---
+++ slingshot.js
@@ -0,0 +1,35 @@
+class Slingshot{
+    constructor(bodyA, point2){
+        var options = {
+            bodyA: bodyA,
+            pointB: point2,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.slingshot = Constraint.create(options);
+        World.add(world, this.slingshot);
+    }
+
+    display(){
+        if(this.slingshot.bodyA)
+        {
+            var pointA = this.slingshot.bodyA.position;
+            var pointB = this.slingshot.pointB;
+            strokeWeight(4);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+        
+    }
+    
+    fly()
+    {
+        this.slingshot.bodyA=null;
+    }
+
+
+    attach(body)
+    {
+        this.slingshot.bodyA=body;
+    }
+}
+
